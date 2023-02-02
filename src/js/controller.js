7b325d4e6caf6cbe9b4dcc789e4c0d1b7c1e05a7
Saleh <wefightwiththem@gmail.com>
2023-02-02 17:16:31 +0330

    Add Update Recipe Servings Feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -55,9 +55,18 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 // Publisher Subscriber
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateService(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -9,82 +9,97 @@ class RecipeView extends View {
   _errorMessage = "We couldn't find that recipe, please try another one";
   _message = "";
 
+  addHandlerUpdateService(handler) {
+    this._parentEl.addEventListener("click", (e) => {
+      const btn = e.target.closest(".btn--increase-servings");
+      if (!btn) return;
+
+      const { updateServ } = btn.dataset;
+      if (+updateServ <= 1) return;
+      handler(+updateServ);
+    });
+  }
+
   _genMarkup() {
     return `
-    <figure class='recipe__fig'>
-      <img src='${this._data.imageUrl}' alt='${
+    <figure class="recipe__fig">
+      <img src="${this._data.imageUrl}" alt="${
       this._data.title
-    }' class='recipe__img' />
-      <h1 class='recipe__title'>
+    }" class="recipe__img" />
+      <h1 class="recipe__title">
         <span>${this._data.title}</span>
       </h1>
     </figure>
 
-    <div class='recipe__details'>
-      <div class='recipe__info'>
-        <svg class='recipe__info-icon'>
-          <use href='${icons}#icon-clock'></use>
+    <div class="recipe__details">
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${icons}#icon-clock"></use>
         </svg>
-        <span class='recipe__info-data recipe__info-data--minutes'>${
+        <span class="recipe__info-data recipe__info-data--minutes">${
           this._data.cookingTime
         }</span>
-        <span class='recipe__info-text'>minutes</span>
+        <span class="recipe__info-text">minutes</span>
       </div>
-      <div class='recipe__info'>
-        <svg class='recipe__info-icon'>
-          <use href='${icons}#icon-users'></use>
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${icons}#icon-users"></use>
         </svg>
-        <span class='recipe__info-data recipe__info-data--people'>${
+        <span class="recipe__info-data recipe__info-data--people">${
           this._data.servings
         }</span>
-        <span class='recipe__info-text'>servings</span>
+        <span class="recipe__info-text">servings</span>
 
-        <div class='recipe__info-buttons'>
-          <button class='btn--tiny btn--increase-servings'>
+        <div class="recipe__info-buttons">
+          <button data-update-serv="${
+            this._data.servings - 1
+          }" class="btn--tiny btn--increase-servings">
             <svg>
-              <use href='${icons}#icon-minus-circle'></use>
+              <use href="${icons}#icon-minus-circle"></use>
             </svg>
           </button>
-          <button class='btn--tiny btn--increase-servings'>
+          <button data-update-serv="${
+            this._data.servings + 1
+          }" class="btn--tiny btn--increase-servings">
             <svg>
-              <use href='${icons}#icon-plus-circle'></use>
+              <use href="${icons}#icon-plus-circle"></use>
             </svg>
           </button>
         </div>
       </div>
 
-      <div class='recipe__user-generated'></div>
-      <button class='btn--round'>
-        <svg class=''>
-          <use href='${icons}#icon-bookmark-fill'></use>
+      <div class="recipe__user-generated"></div>
+      <button class="btn--round">
+        <svg class="">
+          <use href="${icons}#icon-bookmark-fill"></use>
         </svg>
       </button>
     </div>
 
-    <div class='recipe__ingredients'>
-      <h2 class='heading--2'>Recipe ingredients</h2>
-      <ul class='recipe__ingredient-list'>
+    <div class="recipe__ingredients">
+      <h2 class="heading--2">Recipe ingredients</h2>
+      <ul class="recipe__ingredient-list">
         ${this._data.ingredients.map(this._genMarkupIng).join("")}
       </ul>
     </div>
 
-    <div class='recipe__directions'>
-      <h2 class='heading--2'>How to cook it</h2>
-      <p class='recipe__directions-text'>
+    <div class="recipe__directions">
+      <h2 class="heading--2">How to cook it</h2>
+      <p class="recipe__directions-text">
         This recipe was carefully designed and tested by
-        <span class='recipe__publisher'>${
+        <span class="recipe__publisher">${
           this._data.publisher
         }</span>. Please check out
         directions at their website.
       </p>
       <a
-        class='btn--small recipe__btn'
-        href='${this._data.sourceUrl}'
-        target='_blank'
+        class="btn--small recipe__btn"
+        href="${this._data.sourceUrl}"
+        target="_blank"
       >
         <span>Directions</span>
-        <svg class='search__icon'>
-          <use href='${icons}#icon-arrow-right'></use>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
         </svg>
       </a>
     </div>
@@ -97,15 +112,15 @@ class RecipeView extends View {
 
   _genMarkupIng(ing) {
     return `
-      <li class='recipe__ingredient'>
-            <svg class='recipe__icon'>
-              <use href='${icons}#icon-check'></use>
+      <li class="recipe__ingredient">
+            <svg class="recipe__icon">
+              <use href="${icons}#icon-check"></use>
             </svg>
-            <div class='recipe__quantity'>${
+            <div class="recipe__quantity">${
               ing.quantity ? new Fraction(ing.quantity).toString() : ""
             }</div>
-            <div class='recipe__description'>
-              <span class='recipe__unit'>${ing.unit}</span>
+            <div class="recipe__description">
+              <span class="recipe__unit">${ing.unit}</span>
               ${ing.description}
             </div>
           </li>
