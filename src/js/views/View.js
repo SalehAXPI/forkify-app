be0cbd3433b1fd8b88048af05d40c034a060d7f3
Saleh Abbasi <wefightwiththem@gmail.com>
2023-02-05 19:59:20 +0330

    Fix a bug related to "Add bookmark" feature

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,7 +4,7 @@ export default class View {
   _data;
 
   render(data, render = true) {
-    if (!data || data.length === 0) throw new Error();
+    if (!data || data.length === 0) return this.renderError();
 
     this._data = data;
     const markup = this._genMarkup();
