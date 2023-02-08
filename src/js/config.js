be29f226c87f3b8f64a822eebc1b346c2f495cda
Saleh Abbasi <wefightwiththem@gmail.com>
2023-02-08 13:24:10 +0330

    Change api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "089b0579-903c-4dce-aa73-5ba0b7f0fdb5";
+export const KEY = "3c100004-708c-46b6-a6e6-474239a54960";
 export const MODAL_CLOSE_SEC = 2.5;
