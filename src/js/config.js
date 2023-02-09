49f6f7834061a0cae3d39a207779d75087090478
Saleh Abbasi <wefightwiththem@gmail.com>
2023-02-09 21:44:09 +0330

    Add JSDoc documentation example & minor changes

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = "3c100004-708c-46b6-a6e6-474239a54960";
-export const MODAL_CLOSE_SEC = 1;
+export const MODAL_CLOSE_SEC = 0.75;
